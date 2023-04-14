6ee7bcd923e78c876b6ced076c1b205caf713b70
SamProto <SamProto@users.noreply.github.com>
2023-04-14 13:31:59 -0400

    Create Podcast.py

---
+++ Podcast.py
@@ -0,0 +1,4 @@
+#################################
+# Sam Proto Lianna Laricci
+# 4-14-23
+# GPT Podcast: A program designed to automatically generate a podcast between multiple instances of chat GPT
